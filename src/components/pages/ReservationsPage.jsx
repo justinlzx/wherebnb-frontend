e9c6405a14e2302adc028e13742884712a0463a1
ulteare <jonte.lee.2022@scis.smu.edu.sg>
2024-03-11 10:45:21 +0800

    added fields

--- src/components/pages/ReservationsPage.jsx
+++ src/components/pages/ReservationsPage.jsx
@@ -13,11 +13,36 @@ export const ReservationsPage = () => {
                     Request to book
                 </h1>
             </div>
-            <div className="border border-red-500 grid grid-cols-2 rounded-lg p-4 px-16">
+            <div className="border border-red-500 grid grid-cols-2 gap-8 rounded-lg p-4 px-16">
+                {/* 2 halves contained here */}
                 <div className="border border-green-400">
                     <h1 className="font-bold text-2xl">
                         Your trip
                     </h1>
+                    <div className="mt-4">
+                        <div className="flex justify-between">
+                            <h1 className="font-bold">
+                                Dates
+                            </h1>
+                            <button>Edit</button>
+                        </div>
+                        <h1 className="font-semibold mt-1">
+                            {/* To be replaced with dynamic data */}
+                            11–16 Mar
+                        </h1>
+                    </div>
+                    <div className="mt-4">
+                        <div className="flex justify-between">
+                            <h1 className="font-bold mt-1">
+                                Guests
+                            </h1>
+                            <button>Edit</button>
+                        </div>
+                        <h1 className="font-semibold">
+                            {/* To be replaced with dynamic data */}
+                            1 Guest
+                        </h1>
+                    </div>
                 </div>
                 <div className="border border-amber-600">hi 1 </div>
             </div>
