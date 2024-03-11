73ecdf888d0f1b2f20f02904416d5b7ddda0ed1a
ulteare <jonte.lee.2022@scis.smu.edu.sg>
2024-03-11 16:35:02 +0800

    Update ReservationsPage.jsx

--- src/components/pages/ReservationsPage.jsx
+++ src/components/pages/ReservationsPage.jsx
@@ -13,38 +13,61 @@ export const ReservationsPage = () => {
                     Request to book
                 </h1>
             </div>
-            <div className="border border-red-500 grid grid-cols-2 gap-8 rounded-lg p-4 px-16">
+            <div className=" grid grid-cols-2 gap-8 rounded-lg px-16">
                 {/* 2 halves contained here */}
-                <div className="border border-green-400">
+                <div className="">
                     <h1 className="font-bold text-2xl">
                         Your trip
                     </h1>
-                    <div className="mt-4">
+                    <div className="mt-8">
                         <div className="flex justify-between">
-                            <h1 className="font-bold">
-                                Dates
-                            </h1>
-                            <button>Edit</button>
+                            <div>
+                                <h1 className="font-bold">
+                                    Dates
+                                </h1>
+                                <h1 className="font-semibold mt-1">
+                                    {/* To be replaced with dynamic data */}
+                                    11–16 Mar
+                                </h1>
+                            </div>
+                            <button className="btn rounded-lg bg-neutral-100">Edit</button>
                         </div>
-                        <h1 className="font-semibold mt-1">
-                            {/* To be replaced with dynamic data */}
-                            11–16 Mar
-                        </h1>
                     </div>
-                    <div className="mt-4">
+                    <div className="mt-6">
                         <div className="flex justify-between">
-                            <h1 className="font-bold mt-1">
-                                Guests
-                            </h1>
-                            <button>Edit</button>
+                            <div>
+                                <h1 className="font-bold">
+                                    Guests
+                                </h1>
+                                <h1 className="font-semibold mt-1">
+                                    {/* To be replaced with dynamic data */}
+                                    1 Guest
+                                </h1>
+                            </div>
+                            <button className="btn rounded-lg bg-neutral-100">Edit</button>
                         </div>
-                        <h1 className="font-semibold">
-                            {/* To be replaced with dynamic data */}
-                            1 Guest
-                        </h1>
+                    </div>
+                    <hr className="mt-10 "/>
+                    {/* below the line */}
+                    <div className="mt-10 rounded-lg bg-neutral-100 h-40 text-center font-bold">
+                        {/* call Stripe API */}
+                        Stripe API <br />
+                        OR Payment Details if Stripe cannot be called here
                     </div>
                 </div>
-                <div className="border border-amber-600">hi 1 </div>
+                {/* Second Half */}
+                <div className="flex justify-center">
+                    <div className="card w-4/5 bg-base-100 border-2 border-neutral-100 ">
+                        <figure className="h-52"><img src="https://source.unsplash.com/WLUHO9A_xik/200x200" alt="specific" className="w-full"/></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
